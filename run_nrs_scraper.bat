79ca81a9483d5161003147ff991cc2c44445a4a5
nikned-kadena <nikola.nedeljkovic@gmail.com>
2026-06-25 17:54:52 +0200

    Update run_nrs_scraper.bat

--- run_nrs_scraper.bat
+++ run_nrs_scraper.bat
@@ -1,3 +1,10 @@
 @echo off
 cd /d C:\bnv-tracker\bnv-tracker
+
+REM Povuci najnoviju verziju koda i podataka pre pokretanja
+git stash -u >> logs\nrs_scraper.log 2>&1
+git pull --rebase origin main >> logs\nrs_scraper.log 2>&1
+git stash pop >> logs\nrs_scraper.log 2>&1
+
+REM Pokreni scraper (sam radi git push na kraju)
 "C:\Users\NikolaNedeljkovic\AppData\Local\Python\bin\python.exe" scraper\scrape_nrs_playwright.py >> logs\nrs_scraper.log 2>&1
